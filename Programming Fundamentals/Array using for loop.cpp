dbc9c0bc536d0e3bcc5761d445eea767ad70320a
Istafa-Malik <96797199+Istafa-Malik@users.noreply.github.com>
2022-12-05 10:40:00 +0500

    Add files via upload

---
+++ Programming Fundamentals/Array using for loop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x=5;
+	int marks[x];
+	for(int i=0;i<5;i++)
+	{
+		cin>>marks[x];
+	}
+	for(int i=5;i<5;i++)
+	{
+		cout<<marks[x];
+	}
+	system("pause");
+	return 0;
+}
